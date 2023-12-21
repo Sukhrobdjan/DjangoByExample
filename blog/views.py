4403b36356ff88de38a26fcc651a8d45a7d02782
Suxrob <suxidev@gmail.com>
2023-12-21 19:27:23 +0500

    feat: added send post by email servse

--- blog/views.py
+++ blog/views.py
@@ -3,10 +3,48 @@ from django.shortcuts import render, get_object_or_404
 from blog.models import Post
 from django.http import Http404
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from .forms import EmailPostForm
+from django.core.mail import send_mail 
+
+
+
+
+
+
+def post_share(request, post_id):
+    """
+    Recommend posts via email
+    """
+
+    post = get_object_or_404(Post, id = post_id, status = Post.Status.PUBLISHED)
+    if request.method == 'POST':
+        form = EmailPostForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            post_url = request.build_absolute_url(post.get_absolute_url())
+            subject = f"{cd['name']} recommends you read {post.title}"
+            message = f"Read {post.title} at {post_url} \n \n {cd['name']} comment {cd['comments']}"  
+            send_mail(subject, message, "salomsukhrob@gmail.com", [cd['to']])
+            sent = True
+    else:
+        form = EmailPostForm()
+
+    context = {
+         'post':post,
+         'form':form,
+         'sent':sent,
+     }
+    
+    return render(request, 'share.html', context)
+
+
 
 
 
 def post_list(request):
+    """
+    All posts in db 
+    """
 
     post_lists = Post.published.all()
 
@@ -27,6 +65,8 @@ def post_list(request):
     }
 
     return render(request, 'list.html', context)
+
+
 
 
 
